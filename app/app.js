5e330fa42c87ffb71677b99a499b277c86e3b77c
fika2045 <tlpr@naver.com>
2022-06-24 12:09:46 +0900

    주석 추가

--- app/app.js
+++ app/app.js
@@ -5,13 +5,14 @@ const app = express();
 
 //const PORT = 3000;
 
-//라우팅
+//라우팅 : 주소를 받아서 routes/home 으로 보내서 열어줘야하는 화면 열어줌
 const home = require("./src/routes/home");
 
-//앱 세팅
+//앱 세팅 뷰의 위치 설정
 app.set("views", "./src/views");
 app.set("view engine", "ejs");
 
-app.use("/", home); // use -> 미들 웨어를 등록해주는 메서드
+// use -> 미들 웨어를 등록해주는 메서드
+app.use("/", home); 
 
 module.exports = app;
--- app/src/routes/home/index.js
+++ app/src/routes/home/index.js
@@ -5,8 +5,8 @@ const router = express.Router();
 
 const ctrl = require("./home.ctrl");
 
+//호출된 주소에 따라 home.ctrl.js 의 render 함수 호출
 router.get("/", ctrl.home);
-
 router.get("/login", ctrl.login);
 
 module.exports = router; 
