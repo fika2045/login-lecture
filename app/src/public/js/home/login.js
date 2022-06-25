c2a50770a205ac54750f73e6a26d05393667f7f7
fika2045 <tlpr@naver.com>
2022-06-25 23:52:02 +0900

    프런트 로그인 기능 구현

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -22,7 +22,16 @@ fetch("/login", {
    },
    body: JSON.stringify(req),
 })
-.then((res) => res.json())
-.then(console.log);
+    .then((res) => res.json())
+    .then((res) => {
+        if (res.success){
+            location.href ="/";
+        } else {
+            alert(res.msg);
+        }
+    })
+    .catch((err) => {
+        console.error("로그인 중 에러 발생");
+    });
 }
 
