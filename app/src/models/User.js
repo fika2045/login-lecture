e16c10f1c67e6f4d80f9287dd0508dd61fef321b
fika2045 <tlpr@naver.com>
2022-06-26 15:40:13 +0900

    fs 모듈을 통한 로그인 기능 구현

--- app/src/models/User.js
+++ app/src/models/User.js
@@ -6,9 +6,12 @@ class User {
     constructor(body) {
         this.body = body;
     }
-    login() {
+    async login() {
         const client = this.body;
-        const {id, psword} = UserStorage.getUserInfo(client.id);
+        //promise 객체가 데이터를 다 받아오기전에 실핼되는 것을 막기 위해 await 을 씀. 
+        //await 은 promise 객체에서만 쓸수 있다.
+        //또한 await 은 async 함수에서만 사용가능해서 login 을 async 함수로 선언함
+        const {id, psword} = await UserStorage.getUserInfo(client.id);
 
         if(id){
             if (id === client.id && psword === client.psword){
@@ -24,7 +27,7 @@ class User {
     register() {
         const client = this.body;
         const response = UserStorage.save(client);
-        return response;
+        return response; 
     }
 }
 
--- app/src/models/UserStorage.js
+++ app/src/models/UserStorage.js
@@ -1,18 +1,31 @@
 "use strict";
 
+const fs = require("fs").promises;
+
 class UserStorage {
+    static #getUserInfo(data, id) {
+        const users = JSON.parse(data);
+        const idx = users.id.indexOf(id);
+        const usersKeys = Object.keys(users);
+        const userInfo = usersKeys.reduce((newUser, info) =>{
+        newUser[info] = users[info][idx];
+        return newUser;
+    }, {});
+
+    return userInfo;
+    }
     // '#' 은 private 로 선언하기 위한것  
-    static #users = {
-        id: ["tlpr", "finnala", "jiwoo0815"],
-        psword: ["1234", "12345", "123456"],
-        name: ["김승재", "조한주", "김지우"],
-};
+//     static #users = {
+//         id: ["tlpr", "finnala", "jiwoo0815"],
+//         psword: ["1234", "12345", "123456"],
+//         name: ["김승재", "조한주", "김지우"],
+// };
 
 //받아오는 변수 갯수를 모를때 ...변수명
 //이부분 로직을 나중에 확인 필요함 ##############
 //지금은 사용하는 곳이 없음
 static getUsers(...fields) {
-    const users = this.#users;
+    // const users = this.#users;
     const newUsers = fields.reduce((newUsers, field) => {
         if(users.hasOwnProperty(field)) {
  //           console.log(newUsers[field], users[field], field);
@@ -25,23 +38,22 @@ static getUsers(...fields) {
 }
 
     static getUserInfo(id) {
-        const users = this.#users;
-        const idx = users.id.indexOf(id);
-        const usersKeys = Object.keys(users);
-        const userInfo = usersKeys.reduce((newUser, info) =>{
-            newUser[info] = users[info][idx];
-            return newUser;
-        }, {});
-
-        return userInfo;
+        // const users = this.#users;
+        return fs.readFile("./src/databases/users.json")
+        .then((data) => {
+            return this.#getUserInfo(data, id);
+        })
+        .catch(console.error);
+        
     }
 
+    
     static save(userInfo){
-        const users = this.#users;
+        // const users = this.#users;
         users.id.push(userInfo.id);
         users.name.push(userInfo.name);
         users.psword.push(userInfo.psword);
-        console.log(users);
+        return { success: true};
     }
 }
 
