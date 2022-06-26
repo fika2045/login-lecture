6ddf8a37ca2a84a7f630386bc9f93d2fee7ff002
fika2045 <tlpr@naver.com>
2022-06-26 10:51:05 +0900

    회원가입 페이지 구현

--- app/src/routes/home/index.js
+++ app/src/routes/home/index.js
@@ -8,6 +8,9 @@ const ctrl = require("./home.ctrl");
 //호출된 주소에 따라 home.ctrl.js 의 render 함수 호출
 router.get("/", ctrl.output.home);
 router.get("/login", ctrl.output.login);
+router.get("/register", ctrl.output.register);
+
+
 router.post("/login", ctrl.process.login);
 
 module.exports = router; 
